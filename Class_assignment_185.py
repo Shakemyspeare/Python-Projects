4365f143396addd7e1f26a3393c3a9471c52da1b
Shakemyspeare <60899880+Shakemyspeare@users.noreply.github.com>
2020-08-28 07:16:55 -0700

    Python Course
    Step 185

---
+++ Class_assignment_185.py
@@ -0,0 +1,16 @@
+class Drink:
+    name = ''
+    temp = ''
+    containerSize = ''
+    toGo = False
+
+class Coffee(Drink):
+    caffine = True
+    cream = True
+    sugar = True
+
+class Tea(Drink):
+    flavor = ''
+    teaBag = True
+    
+    
